d28d1474825fd19c1227839531c96270ca32ff73
Rivaldz <rivalzsetyo@gmail.com>
2019-12-14 20:55:26 +0700

    menambahkan inten

--- app/src/main/java/com/valdo/fileku/Activities/LoginActivity.java
+++ app/src/main/java/com/valdo/fileku/Activities/LoginActivity.java
@@ -2,15 +2,27 @@ package com.valdo.fileku.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.valdo.fileku.R;
 
 public class LoginActivity extends AppCompatActivity {
+    private TextView daftar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        daftar = findViewById(R.id.registerLoginView);
+
+        daftar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
     }
 }
