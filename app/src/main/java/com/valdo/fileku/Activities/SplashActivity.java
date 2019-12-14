afa9a36f2bd06332c3aa3bf839cdebb1af7a6cf5
Rivaldz <rivalzsetyo@gmail.com>
2019-12-14 20:14:26 +0700

    menambahkan navigation di main activity

---
+++ app/src/main/java/com/valdo/fileku/Activities/SplashActivity.java
@@ -0,0 +1,36 @@
+package com.valdo.fileku.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.valdo.fileku.MainActivity;
+import com.valdo.fileku.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try {
+            this.getSupportActionBar().hide();
+        }
+        catch (NullPointerException e) {
+            setContentView(R.layout.activity_splash);
+        }
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+//                getSupportFragmentManager().beginTransaction().add(R.id.container, fragment).commit();
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        },1500);
+        setContentView(R.layout.activity_splash);
+    }
+}
--- app/src/main/java/com/valdo/fileku/MainActivity.java
+++ app/src/main/java/com/valdo/fileku/MainActivity.java
@@ -1,14 +1,43 @@
 package com.valdo.fileku;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener
+{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        loadFragment(new Load)
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+
+    }
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .addToBackStack(null)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        return false;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
