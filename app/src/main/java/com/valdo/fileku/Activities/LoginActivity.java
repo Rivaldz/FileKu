c47ed2766b5bbde5cb0167605f0cc8cf5d029eb9
Rivaldz <rivalzsetyo@gmail.com>
2019-12-14 20:39:46 +0700

    membuat login activity

---
+++ app/src/main/java/com/valdo/fileku/Activities/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.valdo.fileku.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.valdo.fileku.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
