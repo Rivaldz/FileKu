2ef9f0c1baa904f6ab5c1bd4e321555529114530
Rivaldz <rivalzsetyo@gmail.com>
2019-12-16 05:36:47 +0700

    membuat login dan register firebase auth

--- app/src/main/java/com/valdo/fileku/Activities/LoginActivity.java
+++ app/src/main/java/com/valdo/fileku/Activities/LoginActivity.java
@@ -1,27 +1,66 @@
 package com.valdo.fileku.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.valdo.fileku.MainActivity;
 import com.valdo.fileku.R;
 
 public class LoginActivity extends AppCompatActivity {
     private TextView daftar;
+    private EditText email, pass;
+    private Button loginBut;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         daftar = findViewById(R.id.registerLoginView);
+        email = findViewById(R.id.emailLogin);
+        pass = findViewById(R.id.passLogin);
+        loginBut = findViewById(R.id.buttonLogin);
+
+        final String emailSt = email.getText().toString();
+        final String passSt = pass.getText().toString();
 
         daftar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+
+        loginBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               firebaseAuth.signInWithEmailAndPassword(emailSt, passSt).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                   @Override
+                   public void onComplete(@NonNull Task<AuthResult> task) {
+                       if (task.isSuccessful()){
+                           startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                           Toast.makeText(LoginActivity.this, "Selamat Login Berhasil", Toast.LENGTH_LONG).show();
+                       }
+                       else {
+
+                           Toast.makeText(LoginActivity.this, "Gagal Login Cek Username dan Password", Toast.LENGTH_LONG).show();
+                       }
+
+                   }
+               });
             }
         });
     }
--- app/src/main/java/com/valdo/fileku/Activities/RegisterActivity.java
+++ app/src/main/java/com/valdo/fileku/Activities/RegisterActivity.java
@@ -1,16 +1,48 @@
 package com.valdo.fileku.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.valdo.fileku.R;
 
+import static android.text.TextUtils.isEmpty;
+
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText username, password, email;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        username = findViewById(R.id.usernameDaftar);
+        password = findViewById(R.id.passDaftar);
+        email = findViewById(R.id.emailDaftar);
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        String emailString = email.getText().toString();
+        String passString = password.getText().toString();
+        if (!isEmpty(emailString) && !isEmpty(passString)){
+            firebaseAuth.createUserWithEmailAndPassword(emailString,passString).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    Toast.makeText(getBaseContext(), "Selamat Registrasi anda berhasil", Toast.LENGTH_LONG).show();
+                    startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+                }
+            });
+        }
+
+
     }
 }
